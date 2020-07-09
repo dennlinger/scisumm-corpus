848c8ebcbb242aab09a8c84c71e1d68e62345fa7
Dennis Aumiller <dennis.aumiller@gmail.com>
2020-07-09 16:13:54 +0200

    Adding current state of project.

--- solr_commit_test.py
+++ solr_commit_test.py
@@ -1,48 +1,55 @@
 from lxml import etree
+from tqdm import tqdm
+import subprocess
 import pysolr
 import os
-import re
 
 
 if __name__ == "__main__":
-    solr = pysolr.Solr('http://localhost:8983/solr/scisumm/', always_commit=True)
 
-    solr.ping()
+    for filename in tqdm(sorted(os.listdir("./data/Training-Set-2019/Task1/From-Training-Set-2018/"))):
 
-    # solr.add([
-    #     {
-    #         "id": "1",
-    #         "ssid": "1",
-    #         "reference_section": "Abstract",
-    #         "text": "This is a first test."
-    #     },
-    # ])
+        subprocess.call(["/home/dennis/solr-8.5.2/bin/solr", "create_core", "-c", filename, "-d",
+                         "/home/dennis/solr-8.5.2/server/solr/configsets/scisumm/"])
 
-    base_path = "./data/Training-Set-2019/Task1/From-Training-Set-2018/" + "C00-2123"
-    ref_xml = os.path.join(base_path, "Reference_XML", "C00-2123.xml")
-    tree = etree.parse(ref_xml, parser=etree.XMLParser(encoding='ISO-8859-1', recover=True))
-    root = tree.getroot()
+        solr = pysolr.Solr('http://localhost:8983/solr/' + filename + '/', always_commit=True)
 
-    sentences = tree.xpath(".//S")
+        solr.ping()
 
-    for sentence in sentences:
-        try:
-            ssid = sentence.attrib["ssid"]
-        except KeyError:
-            ssid = "0"  # Only happens for title.
+        # solr.add([
+        #     {
+        #         "id": "1",
+        #         "ssid": "1",
+        #         "reference_section": "Abstract",
+        #         "text": "This is a first test."
+        #     },
+        # ])
 
-        # Reference sections for PAPER (title) and ABSTRACT are different
-        try:
-            reference_section = sentence.getparent().attrib["title"]
-        except KeyError:
-            reference_section = sentence.getparent().tag
+        base_path = "./data/Training-Set-2019/Task1/From-Training-Set-2018/" + filename
+        ref_xml = os.path.join(base_path, "Reference_XML", filename + ".xml")
+        tree = etree.parse(ref_xml, parser=etree.XMLParser(encoding='ISO-8859-1', recover=True))
+        root = tree.getroot()
 
-        # clean_text = re.sub(r"\+-(&&)\|\|!\(\)\{\}\[\]\^\"~\*\?:\\/", "", sentence.text)
-        clean_text = sentence.text
-        solr.add([
-            {
-                "id": sentence.attrib["sid"],
-                "ssid": ssid,
-                "reference_section": reference_section,
-                "text": clean_text
-            }])
+        sentences = tree.xpath(".//S")
+
+        for sentence in sentences:
+            try:
+                ssid = sentence.attrib["ssid"]
+            except KeyError:
+                ssid = "0"  # Only happens for title.
+
+            # Reference sections for PAPER (title) and ABSTRACT are different
+            try:
+                reference_section = sentence.getparent().attrib["title"]
+            except KeyError:
+                reference_section = sentence.getparent().tag
+
+            # clean_text = re.sub(r"\+-(&&)\|\|!\(\)\{\}\[\]\^\"~\*\?:\\/", "", sentence.text)
+            clean_text = sentence.text
+            solr.add([
+                {
+                    "id": sentence.attrib["sid"],
+                    "ssid": ssid,
+                    "reference_section": reference_section,
+                    "text": clean_text
+                }])
--- solr_query_test.py
+++ solr_query_test.py
@@ -1,30 +1,50 @@
 from collections import defaultdict
 from bs4 import BeautifulSoup
+from tqdm import tqdm
 import pysolr
 import re
+import os
 
 from exploration import get_citances_for_file
 
 
 if __name__ == "__main__":
-    solr = pysolr.Solr('http://localhost:8983/solr/scisumm/', always_commit=True)
+    top_k = 5
+    tp = 0
+    exact_tp = 0
+    overall = 0
+    for filename in tqdm(sorted(os.listdir("./data/Training-Set-2019/Task1/From-Training-Set-2018/"))):
+        solr = pysolr.Solr('http://localhost:8983/solr/' + filename + '/', always_commit=True)
 
-    solr.ping()
+        solr.ping()
 
-    citances = get_citances_for_file("C00-2123", list())
+        citances = get_citances_for_file(filename, list())
 
-    for citance in citances:
-        results = defaultdict(float)
-        all_query_sentences = citance["Citation Text"]
-        soup = BeautifulSoup(all_query_sentences, "html.parser")
-        truth = set(citance["Reference Offset"])
-        texts = soup.findAll()
-        for el in texts:
-            clean_text = re.sub(r"([\+\-(&&)\|\|!\(\)\{\}\[\]\^\"\~\*\?:\\\/])", r"\\\1", el.text)
-            # print(clean_text)
-            res = solr.search(clean_text, fl="id, score")
+        for citance in citances:
+            results = defaultdict(float)
+            all_query_sentences = citance["Citation Text"]
+            soup = BeautifulSoup(all_query_sentences, "html.parser")
+            truth = set(citance["Reference Offset"])
+            texts = soup.findAll()
+            clean_text = ""
+            for el in texts:
+                clean_text += re.sub(r"([\+\-(&&)\|\|!\(\)\{\}\[\]\^\"\~\*\?:\\\/])", r"\\\1", el.text)
+                # print(clean_text)
+
+            res = solr.search(clean_text, fl="id, score", rows=top_k)
+            print(res.docs, len(truth))
             for doc in res.docs:
                 results[doc["id"]] += doc["score"]
 
-        print(results)
-        print(truth)
+            # print(results)
+            # print(truth)
+            # Total number of samples is equal to annotations in truth.
+            overall += len(truth)
+            # Calculate coverage of current top k results
+            tp += len(set(results.keys()).intersection(truth))
+            exact_tp += len(set(list(results.keys())[:len(truth)]).intersection(truth))
+
+
+    print(f"Recall @ {top_k} for the whole dataset: {tp / overall:.4f}")
+    print(f"Precision for the whole dataset: {exact_tp / overall:.4f}")
+
